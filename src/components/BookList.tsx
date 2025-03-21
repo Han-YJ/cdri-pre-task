9a4c4e19e7a7b3c32a27dce5b88264c8ac22305b
yjhan <yjhan@beaverworksinc.com>
2025-03-21 14:50:50 +0900

    feat: BookList component 추가
    도서검색 리스트

---
+++ src/components/BookList.tsx
@@ -0,0 +1,22 @@
+import { useSearchBooks } from '@/hooks/useSearchBooks';
+import ListTitle from './shared/ListTitle';
+import List from './shared/List';
+import BookListItem from './BookListItem';
+import NoData from './shared/NoData';
+
+const BookList = () => {
+  const { data, metaData } = useSearchBooks();
+  return (
+    <div className="flex flex-col">
+      <ListTitle title="도서 검색 결과" totCnt={metaData?.total_count ?? 0} className="mb-9" />
+
+      {data?.length === 0 ? (
+        <NoData title="검색된 결과가 없습니다." className="py-20" />
+      ) : (
+        <List data={data} renderItem={(book) => <BookListItem data={book} />} />
+      )}
+    </div>
+  );
+};
+
+export default BookList;
