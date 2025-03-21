d2e6f50576bd120c579f62ee6e496a78d2d0dac9
yjhan <yjhan@beaverworksinc.com>
2025-03-21 23:43:49 +0900

    feat: 검색하기 페이지에 검색 및 리스트 추가

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,5 +1,13 @@
+import BookList from '@/components/BookList';
+import SearchBox from '@/components/SearchBox';
+
 const Search = () => {
-  return <div>Search page</div>;
+  return (
+    <div className="flex flex-col gap-6">
+      <SearchBox />
+      <BookList />
+    </div>
+  );
 };
 
 export default Search;
