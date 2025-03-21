e9112c7de6738ad0fb412cd9082967b0eb6164cc
yjhan <yjhan@beaverworksinc.com>
2025-03-21 16:13:04 +0900

    feat: searchHistoryAtom 추가
    검색어 저장 atom

--- src/atoms/searchAtoms.ts
+++ src/atoms/searchAtoms.ts
@@ -1,8 +1,12 @@
 import { atom } from 'jotai';
 import { SearchBooksParams } from '@/types/search';
+import { atomWithStorage } from 'jotai/utils';
 
 export const searchParamsAtom = atom<SearchBooksParams>({
   query: '',
   page: 1,
   target: undefined,
 });
+
+const SEARCH_HISTORY_KEY = 'searchHistory';
+export const searchHistoryAtom = atomWithStorage<string[]>(SEARCH_HISTORY_KEY, []);
