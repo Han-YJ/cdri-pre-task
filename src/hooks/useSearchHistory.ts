6b7613e67460134e9bb6d4bd2efa51c4bedab594
yjhan <yjhan@beaverworksinc.com>
2025-03-21 16:14:34 +0900

    feat: useSearchHistory.ts 추가
    검색어 저장, 삭제 hooks

---
+++ src/hooks/useSearchHistory.ts
@@ -0,0 +1,29 @@
+import { searchHistoryAtom } from '@/atoms/searchAtoms';
+import { useAtom } from 'jotai';
+
+export const useSearchHistory = () => {
+  const [searchHistory, setSearchHistory] = useAtom(searchHistoryAtom);
+
+  // 검색어 추가
+  const addSearchQuery = (newQuery: string) => {
+    setSearchHistory((prevHistory) => {
+      // 새로운 검색어가 있으면 기존 검색어 중복 제거 후 최대 8개까지 저장
+      const updatedHistory = [newQuery, ...prevHistory.filter((term) => term !== newQuery)].slice(
+        0,
+        8
+      );
+      return updatedHistory;
+    });
+  };
+
+  // 검색어 제거
+  const removeSearchQuery = (query: string) => {
+    setSearchHistory((prevHistory) => prevHistory.filter((term) => term !== query));
+  };
+
+  return {
+    searchHistory,
+    addSearchQuery,
+    removeSearchQuery,
+  };
+};
