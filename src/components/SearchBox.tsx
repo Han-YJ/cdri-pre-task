96737708e6f01a8118370a7c0f1dd756afbb144e
yjhan <yjhan@beaverworksinc.com>
2025-03-21 16:59:03 +0900

    feat: SearchBox component 추가
    검색, 검색어 저장 및 삭제 완료

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,52 +1,118 @@
 import { useState } from 'react';
 import { useAtom } from 'jotai';
-import { searchParamsAtom } from '@/atoms/searchAtoms';
+import { Button } from './shared/Button';
 import Typography from './shared/Typography';
+import CloseIcon from '@/icons/close.svg?react';
 import SearchIcon from '@/icons/search.svg?react';
+import { useSearchHistory } from '@/hooks/useSearchHistory';
+import { searchParamsAtom } from '@/atoms/searchAtoms';
+import { cn } from '@/utils/styles';
 
 const SearchBox = () => {
   const [searchParams, setSearchParams] = useAtom(searchParamsAtom);
+  const { searchHistory, addSearchQuery, removeSearchQuery } = useSearchHistory();
   const [query, setQuery] = useState(searchParams.query || '');
+  const [isInputFocused, setIsInputFocused] = useState(false);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(event.target.value);
   };
 
-  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === 'Enter') {
-      setSearchParams({ ...searchParams, query });
-    }
+  const handleSearchQuery = (query: string) => {
+    setSearchParams({ ...searchParams, query });
+    addSearchQuery(query);
+    setIsInputFocused(false);
   };
 
-  const handleDetailSearch = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>): void => {
-    event.preventDefault();
-    console.log('상세검색');
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (query.length === 0) return;
+    if (event.key === 'Enter') handleSearchQuery(query);
+  };
+
+  //상세검색
+  //const handleDetailSearch = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>): void => {
+  //  e.preventDefault();
+  //  console.log('상세검색');
+  //};
+
+  // history
+  const handleSelectHistory = (query: string) => {
+    setQuery(query);
+    handleSearchQuery(query);
+  };
+  const handleFocus = () => {
+    if (searchHistory.length === 0) return;
+    setIsInputFocused(true);
+  };
+  const handleBlur = () => {
+    setIsInputFocused(false);
   };
 
   return (
-    <div className="flex flex-col gap-4 max-w-[568px] mx-auto">
+    <div className="flex min-h-[100px] max-w-[568px] flex-col gap-4">
       <Typography as="h2" variant="title2" className="text-black">
         도서 검색
       </Typography>
 
-      <div className="flex items-center space-x-2 flex-grow">
-        {/* 검색 input */}
-        <div className="flex items-center p-2.5 gap-2.5 w-[480px] rounded-full bg-lightGray placeholder:text-base placeholder:leading-4">
-          <SearchIcon />
-          <input
-            type="text"
-            value={query}
-            onChange={handleInputChange}
-            onKeyDown={handleKeyPress}
-            placeholder="검색어를 입력하세요"
-            className="w-full outline-none focus:outline-none"
-          />
-        </div>
+      <div className="relative flex-grow justify-start">
+        <div className="absolute top-0 left-0 z-10 flex gap-2">
+          <div
+            className={cn(
+              'bg-lightGray w-[480px] p-2.5 placeholder:text-base placeholder:leading-4',
+              isInputFocused ? 'rounded-3xl pb-6' : 'rounded-full'
+            )}
+          >
+            {/* 검색 input */}
+            <div className="flex items-center gap-5">
+              <SearchIcon />
+              <input
+                type="text"
+                value={query}
+                onChange={handleInputChange}
+                onKeyDown={handleKeyPress}
+                placeholder="검색어를 입력하세요"
+                className="w-full outline-none focus:outline-none"
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+              />
+            </div>
 
-        {/* 상세검색 button*/}
-        <button onClick={handleDetailSearch} className="text-sm">
-          상세검색
-        </button>
+            {/* history 표시 */}
+            {isInputFocused && searchHistory.length > 0 && (
+              <ul
+                onMouseDown={(event) => {
+                  event.preventDefault();
+                }}
+                className="ml-12 flex flex-col gap-4 pt-4.5 pr-6"
+              >
+                {searchHistory.map((historyQuery) => (
+                  <li
+                    key={historyQuery}
+                    className="hover:text-text-primary flex cursor-pointer items-center justify-between"
+                    onClick={() => handleSelectHistory(historyQuery)}
+                  >
+                    <Typography variant="caption" color="subtitle">
+                      {historyQuery}
+                    </Typography>
+                    <CloseIcon
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        removeSearchQuery(historyQuery);
+                      }}
+                    />
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+
+          {/* 상세검색 button*/}
+          <div className="flex h-[50px] items-center justify-center">
+            <Button variant="outline" size="sm">
+              상세검색
+            </Button>
+          </div>
+        </div>
       </div>
     </div>
   );
