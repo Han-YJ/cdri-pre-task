a82de7effb7a54cd5eeb37ab6c85a64e8b6b2d45
yjhan <yjhan@beaverworksinc.com>
2025-03-23 16:37:44 +0900

    feat: 내가 찜한 책 페이지 완성

--- src/pages/Like.tsx
+++ src/pages/Like.tsx
@@ -1,5 +1,28 @@
+import BookListItem from '@/components/BookListItem';
+import List from '@/components/shared/List';
+import ListTitle from '@/components/shared/ListTitle';
+import NoData from '@/components/shared/NoData';
+import Typography from '@/components/shared/Typography';
+import { useLikedBooks } from '@/hooks/useLikedBooks';
+
 const Like = () => {
-  return <div>Like</div>;
+  const { likedBooks } = useLikedBooks();
+
+  return (
+    <div>
+      <Typography as="h2" variant="title2" className="mb-6 text-black">
+        내가 찜한 책
+      </Typography>
+
+      <ListTitle title="찜한 책" totCnt={likedBooks.length} className="mb-9" />
+
+      {likedBooks.length === 0 ? (
+        <NoData title="찜한 책이 없습니다." className="py-20" />
+      ) : (
+        <List data={likedBooks} renderItem={(book) => <BookListItem data={book} />} />
+      )}
+    </div>
+  );
 };
 
 export default Like;
