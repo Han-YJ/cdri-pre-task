2e7aadef93a37b720da6f094583ee089e84e46d9
yjhan <yjhan@beaverworksinc.com>
2025-03-23 12:47:24 +0900

    refactor: Dropdown component 타겟부분에 적용

--- src/components/SearchDetailContent.tsx
+++ src/components/SearchDetailContent.tsx
@@ -1,7 +1,6 @@
-import CloseIcon from '@/icons/close.svg?react';
 import { Button } from './shared/Button';
 import Typography from './shared/Typography';
-import ArrowIcon from '@/icons/arrow.svg?react';
+import CloseIcon from '@/icons/close.svg?react';
 
 interface SearchDetailContentProps {
   onClose: () => void;
@@ -10,12 +9,9 @@ import { useState } from 'react';
 import { SearchTarget } from '@/types/search';
 import { useSetAtom } from 'jotai';
 import { searchParamsAtom } from '@/atoms/searchAtoms';
+import Dropdown, { Option } from './shared/Dropdown';
 
-type targetOption = {
-  name: string;
-  value: SearchTarget;
-};
-const targetOptions: targetOption[] = [
+const targetOptions: Option[] = [
   { name: '제목', value: 'title' },
   { name: '저자명', value: 'person' },
   { name: '출판사', value: 'publisher' },
@@ -24,22 +20,16 @@ const targetOptions: targetOption[] = [
 const SearchDetailContent = ({ onClose }: SearchDetailContentProps) => {
   const setSearchParams = useSetAtom(searchParamsAtom);
   const [query, setQuery] = useState('');
-  const [searchTarget, setSearchTarget] = useState<targetOption>(targetOptions[0]);
-  const [isOptionOpen, setIsOptionOpen] = useState(false);
+  const [searchTarget, setSearchTarget] = useState<Option>(targetOptions[0]);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(event.target.value);
   };
 
-  const handleOptionClick = (option: targetOption) => {
-    setSearchTarget(option);
-    setIsOptionOpen(false);
-  };
-
   // 상세 검색하기
   const handleDetailSearch = () => {
     if (query.length === 0) return;
-    setSearchParams((prev) => ({ ...prev, query, target: searchTarget.value }));
+    setSearchParams((prev) => ({ ...prev, query, target: searchTarget.value as SearchTarget }));
     onClose();
   };
 
@@ -50,39 +40,10 @@ const SearchDetailContent = ({ onClose }: SearchDetailContentProps) => {
         className="text-text-secondary absolute top-2 right-2 h-5 w-5 cursor-pointer"
       />
       <div className="mb-4 flex w-full gap-1">
-        <div className="relative inline-block">
-          {/* 커스텀 드롭다운 */}
-          <div className="absolute top-full left-0 w-full bg-white shadow-lg">
-            {isOptionOpen && (
-              <ul className="p-2">
-                {targetOptions.map((option, idx) => (
-                  <li
-                    key={`option_${idx}`}
-                    value={option.value}
-                    onClick={() => handleOptionClick(option)}
-                    className="h-[30px] cursor-pointer"
-                  >
-                    <Typography variant="body2" as="span" color="subtitle">
-                      {option.name}
-                    </Typography>
-                  </li>
-                ))}
-              </ul>
-            )}
-          </div>
-
-          {/* 선택된 옵션을 보여주는 영역 */}
-          <div
-            onClick={() => setIsOptionOpen(!isOptionOpen)}
-            className="border-b-border-gray flex h-9 w-25 cursor-pointer items-center justify-between border-b p-2"
-          >
-            <Typography variant="body2" as="span" bold>
-              {searchTarget.name}
-            </Typography>
-
-            <ArrowIcon />
-          </div>
+        <div className="w-25">
+          <Dropdown options={targetOptions} selected={searchTarget} onSelect={setSearchTarget} />
         </div>
+
         <div className="border-b-primary flex w-full justify-start border-b pl-1">
           <input
             type="text"
