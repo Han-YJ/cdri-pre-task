e019d5ac4598331d41d3e101fe57704ccf0a4b65
yjhan <yjhan@beaverworksinc.com>
2025-03-23 22:50:27 +0900

    fix: 카카오 api 환경변수 오타 수정

--- src/api/kakaoApi.ts
+++ src/api/kakaoApi.ts
@@ -1,10 +1,9 @@
 import axiosInstance from './axiosInstance';
 
 const KAKAO_API_KEY = import.meta.env.VITE_KAKAO_REST_API_KEY;
-const KAKAO_API_BASE_URL = import.meta.env.VITE_KAKAO_API_BASE_URL;
+const KAKAO_API_BASE_URL = import.meta.env.VITE_KAKAO_REST_BASE_URL;
 
 const kakaoApi = axiosInstance;
-
 kakaoApi.defaults.baseURL = KAKAO_API_BASE_URL;
 
 kakaoApi.interceptors.request.use((config) => {
