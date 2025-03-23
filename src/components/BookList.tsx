7b5172a79584bd23111d6e852bef1572b696591f
yjhan <yjhan@beaverworksinc.com>
2025-03-23 13:21:03 +0900

    feat: 도서검색 리스트 무한 스크롤 적용

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -5,7 +5,7 @@ import BookListItem from './BookListItem';
 import NoData from './shared/NoData';
 
 const BookList = () => {
-  const { data, metaData } = useSearchBooks();
+  const { data, metaData, hasNextPage, fetchNextPage } = useSearchBooks();
   return (
     <div className="flex flex-col">
       <ListTitle title="도서 검색 결과" totCnt={metaData?.total_count ?? 0} className="mb-9" />
@@ -13,7 +13,11 @@ const BookList = () => {
       {data?.length === 0 ? (
         <NoData title="검색된 결과가 없습니다." className="py-20" />
       ) : (
-        <List data={data} renderItem={(book) => <BookListItem data={book} />} />
+        <List
+          data={data}
+          renderItem={(book) => <BookListItem data={book} />}
+          onReachEnd={hasNextPage ? fetchNextPage : undefined}
+        />
       )}
     </div>
   );
