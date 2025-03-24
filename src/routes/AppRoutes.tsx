37bb1959068d26364ef62f8228086704c27fe010
yjhan <yjhan@beaverworksinc.com>
2025-03-24 14:58:09 +0900

    refactor: page 단위 lazy loading 적용

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,7 +1,9 @@
 import { Route, Routes } from 'react-router';
 import Layout from '@/components/shared/DefaultLayout';
-import Search from '@/pages/Search';
-import Like from '@/pages/Like';
+import { lazy } from 'react';
+
+const Search = lazy(() => import('@/pages/Search'));
+const Like = lazy(() => import('@/pages/Like'));
 
 function AppRoutes() {
   return (
