4a16b3b3b862394901ce36d8211d2f7c0ad859e4
yjhan <yjhan@beaverworksinc.com>
2025-04-09 17:57:31 +0900

    feat: Button 컴포넌트 스토리 추가

---
+++ src/stories/Button.stories.ts
@@ -0,0 +1,35 @@
+import { Button } from '@/components/shared/Button';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof Button> = {
+  title: 'Example/Button',
+  component: Button,
+  tags: ['autodocs'],
+  argTypes: {
+    variant: {
+      control: 'select',
+      options: ['primary', 'secondary', 'outline'],
+    },
+    size: {
+      control: 'select',
+      options: ['lg', 'md', 'sm', 'full'],
+    },
+    disabled: {
+      control: 'boolean',
+    },
+    onClick: {
+      action: 'clicked',
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+export const Primary: Story = {
+  args: {
+    variant: 'primary',
+    size: 'md',
+    children: 'Button',
+  },
+};
